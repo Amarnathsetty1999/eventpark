cf4379072a8a72a164ab8399e0e75f0cb8bb448b
Amarnathsetty1999 <amsetty19@gmail.com>
2022-11-30 07:41:52 +0000

    changes

--- getDescription.php
+++ getDescription.php
@@ -1,11 +1,11 @@
 <?php
-$pid=$_POST['eventid'];
+$pid=$_POST['pid'];
 $pdo=new PDO("mysql:host=localhost;dbname=park_tickets","root","");
 $result=$pdo->query("select * from events where eventid='$pid'"); 
 $pd="";
 if(($row=$result->fetch()))
 {
-   $pd=$row['pdesc']; 
+   $pd=$row['eventdescription']; 
 }
 $pdo=null;
 echo "<p>$pd</p>";
